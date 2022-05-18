ddbe418362cd811251552b4376ff5ff0002b53a5
salmanroug <68188068+salmanroug@users.noreply.github.com>
2022-05-18 17:06:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+//FIXED HEADER ON SCROLL FUNCTION//
+window.onscroll = function() {fixedHeader()};
+
+var header = document.getElementById("fixed-header");
+const currentLocation = location.href;
+const navItem = document.querySelectorAll(".nav-item");
+
+for (i = 0; i<navItem.length; i++) {
+    if(navItem[i].href === currentLocation) {
+        navItem[i].className = "active-nav";
+    };
+}
+
+var sticky = header.offsetTop;
+
+function fixedHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("fixed-header");
+  } else {
+    header.classList.remove("fixed-header");
+  }
+}
+
+//SKILLBOX TOGGLE FUNCTION//
 const skillbox = document.getElementsByClassName("skillbox");
 
 for (i = 0; i<skillbox.length; i++) {
     skillbox[i].addEventListener("click", function(){
-        this.classList.toggle("active");
-    })
+        this.classList.add("active");
+    });
 }
+
+
